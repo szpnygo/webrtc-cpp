56e87caf775fd8261e8443424d84d7410b9b668f
neosu <neo@tcodestudio.com>
2023-11-22 13:58:42 +0800

    update cmake

--- webrtc-config.cmake
+++
@@ -1,6 +0,0 @@
-@PACKAGE_INIT@
-
-# Include the targets file. This file contains definitions of imported targets.
-if(NOT TARGET webrtc)
-    include("${CMAKE_CURRENT_LIST_DIR}/webrtcTargets.cmake")
-endif()
